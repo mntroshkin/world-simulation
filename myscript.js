b5622b9c868f64bb1e3077f1ebacb1bf487db6cc
mntroshkin <tr2mikh@gmail.com>
2024-12-05 12:36:31 +0300

    added scroll buttons

--- myscript.js
+++ myscript.js
@@ -1,33 +1,11 @@
-function renderTabMenu() {
-  const tabMenu = document.getElementById("tab-menu");
-  while (tabMenu.firstChild) {
-    tabMenu.removeChild(tabMenu.firstChild);
-  }
-
-  openTabs.forEach((tab) => {
-    tab.tabContainer.classList.remove("active");
-  });
-
-  activeTab.tabContainer.classList.add("active");
-
-  openTabs.forEach((tab) => {
-    tabMenu.insertBefore(tab.tabContainer, null);
-  });
-}
-
-
 function renderActiveTab() {
   const rightSection = document.getElementById("right-section");
-  while (rightSection.firstChild) {
-    rightSection.removeChild(rightSection.firstChild);
-  }
-
-  rightSection.insertBefore(activeTab.tabPanel, null);
+  rightSection.innerHTML = activeTab.tab_content;
 }
 
 
 class Tab {
-  constructor(tab_id, tab_name, tab_content) {
+  constructor(tab_id, tab_name, tab_content, closable=true) {
     this.tab_id = tab_id;
     this.tab_name = tab_name;
     this.tab_content = tab_content;
@@ -40,65 +18,66 @@ class Tab {
     this.tabNameEntry.textContent = this.tab_name;
     this.tabContainer.insertBefore(this.tabNameEntry, null);
 
-    this.tabPanel = document.createElement("div");
-    this.tabPanel.classList.add("tab-panel");
-    this.tabPanel.innerHTML = this.tab_content;
-
     this.tabNameEntry.addEventListener('click', () => {this.makeActive()});
+
+    if (closable) {
+      const closeButton = document.createElement("button");
+      closeButton.classList.add("close-button");
+      closeButton.textContent = "×";
+      closeButton.addEventListener('click', () => {this.close()});
+      this.tabContainer.insertBefore(closeButton, null);
+    }
+
+    openedTabs.push(this);
+    this.makeActive();
+    tabWrapper.insertBefore(this.tabContainer, null);
   }
 
   makeActive() {
+    if (activeTab) { activeTab.tabContainer.classList.remove("active"); }
+    this.tabContainer.classList.add("active");
     activeTab = this;
-    renderTabMenu();
     renderActiveTab();
   }
 
-  open() {
-    openTabs.push(this);
-    this.makeActive();
-  }
-}
-
-class ClosableTab extends Tab {
-  constructor(tab_id, tab_name, tab_content) {
-    super(tab_id, tab_name, tab_content);
-
-    this.closeButton = document.createElement("button");
-    this.closeButton.classList.add("close-button");
-    this.closeButton.textContent = "×";
-    this.closeButton.addEventListener('click', () => {this.close()});
-    this.tabContainer.insertBefore(this.closeButton, null);
-  }
-
   close() {
-    let index = openTabs.indexOf(this);
-    openTabs = openTabs.slice(0, index).concat(openTabs.slice(index + 1));
+    this.tabContainer.classList.add("hidden");
 
     if (this == activeTab) {
-      let newActiveTab = undefined;
-      if (index < openTabs.length) {
-        newActiveTab = openTabs[index];
-      }
-      else {
-        newActiveTab = openTabs[index - 1];
-      }
+      const newActiveTab = (index < openedTabs.length) ? openedTabs[index] : openedTabs[index - 1];
       newActiveTab.makeActive();
-    }
-    else {
-      renderTabMenu();
     }
   }
 }
 
-let openTabs = [];
+const tabWrapper = document.getElementById("tab-wrapper");
+let openedTabs = [];
 let activeTab = undefined;
-const mainTab = new Tab("main-tab", "Main Tab", "Hello, this is a main tab!");
-mainTab.open()
+
+const scrollLeft = document.getElementById('scroll-left');
+const scrollRight = document.getElementById('scroll-right');
+
+const mainTab = new Tab("main-tab", "Main Tab", "Hello, this is the main tab!", closable=false);
 
 let counter = 1;
-newTabLink = document.getElementById("new-tab-link");
+const newTabLink = document.getElementById("new-tab-link");
 newTabLink.addEventListener('click', () => {
   counter += 1;
-  newTab = new ClosableTab(`tab${counter}`, `Tab #${counter}`, `Hello, this is tab #${counter}!`);
-  newTab.open();
+  newTab = new Tab(`tab${counter}`, `Tab #${counter}`, `Hello, this is the tab #${counter}!`);
+});
+
+const scrollAmount = 150;
+
+scrollLeft.addEventListener('click', () => {
+    tabWrapper.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth'
+    });
+});
+
+scrollRight.addEventListener('click', () => {
+    tabWrapper.scrollBy({
+          left: scrollAmount,
+          behavior: 'smooth'
+      });
 });
